c0a933d3289e5ce85ec132ac4ffcdd97306d59c3
osmankbk <mackiekbk@yahoo.com>
2021-03-11 17:37:13 -0500

    Adds traffic chart

--- js/app.js
+++ js/app.js
@@ -1,18 +1,40 @@
 //Store alert div
 const alertBanner = document.querySelector(".alert");
 //Store canvas element('traffic-chart')
-const trafficChart = document.querySelector('#traffic-chart');
-console.log(trafficChart);
-//Object containing traffic chart data.
-let traffic = {
-    labels: [],
-    datasets: [{
-        data: [],
-        backgroundColor: '#777',
-        borderwidth: 2,
-    }],
-};
+//const trafficChart = document.querySelector('#traffic-chart');
 
+
+//Object containing traffic chart data.
+const trafficCanvas = document.querySelector('#traffic-chart');
+let trafficData = {
+  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+  datasets: [{
+    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+    backgroundColor: 'rgb(233, 186, 233)',
+    borderWidth: 1,
+  }]
+};
+let trafficOptions = {
+  aspectRatio: 2.5,
+  animation: {
+    duration: 0
+  },
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  },
+  legend: {
+    display: false
+  }
+};
+let trafficChart = new Chart(trafficCanvas, {
+  type: 'line',
+  data: trafficData,
+  options: trafficOptions
+});
 
 //Fuction that gives content to the alert div in the htmll
 const callAlertBanner = () => {
