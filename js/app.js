46a49538a59464e0723e39d4c721061518b1bc85
osmankbk <mackiekbk@yahoo.com>
2021-03-19 18:38:24 -0400

    Adds functionality for header dropdown, traffic buttons & user messager auto-complete

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
 //Store alert div
 const alertBanner = document.querySelector(".alert");
-//Store canvas element('traffic-chart')
-const trafficCanvas = document.querySelector('#traffic-chart');
-//Store canvas element('daily-chart')
-const dailyCanvas = document.querySelector('.daily-chart');
-//Store canvas element('mobile-chart')
-const mobileCanvas = document.querySelector('.mobile-chart');
 //Store notifications div
 const notifications = document.querySelector('#notifications');
 //Bell SVG
@@ -69,13 +63,15 @@ bell.addEventListener('click', () => {
   notifications.style.backgroundColor = 'transparent';
 });
 
-
-// window.addEventListener('click', (e) => {
-//   if(notification.classList.contains('showing')) {
-//     console.log(e.target);
-//     notification.style.display = 'none';
-//   }
-// });
+//Window click event to get raid of notification div when clicked outside of it.
+window.addEventListener('click', (e) => {
+  if(e.target !== bell && notification.classList.contains('showing')) {
+    notification.style.display = 'none';
+  }
+  if (e.target.classList.contains('close')) {
+    notification.style.display = 'block';
+}
+});
 
 
 //Function that gives content to the alert div in the htmll
@@ -97,98 +93,55 @@ alertBanner.addEventListener('click', (e) => {
 });
 
 
-//Objects containing traffic chart data.
-let trafficData = {
-  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-  datasets: [{
-    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-    backgroundColor: 'rgb(233, 186, 233)',
-    borderWidth: 1,
-  }]
-};
-let trafficOptions = {
-  aspectRatio: 2.5,
-  animation: {
-    duration: 0
-  },
-  scales: {
-    yAxes: [{
-      ticks: {
-        beginAtZero: true
-      }
-    }]
-  },
-  legend: {
-    display: false
+//Auto complete section
+//Get required elements
+//Users info
+const userInfo = ['Victoria Chambers', 'Josh Camron', 'Alyssa Finn', 'Readdy Yung', 'Caprice Laws', 'Adam Gouveia', 'Arminata Kamara'];
+//Form input
+const input = document.querySelector('#message-form');
+//list container
+const usersContainer = document.querySelector('.suggest-list');
+//Form input
+const formInput = document.querySelector('.message-form');
+//Appended li elements
+
+
+
+
+
+//If key up
+input.addEventListener('keyup', (e) => {
+  //Store input typed users
+  let users = [];
+  let inputValue = e.target.value;
+  if(inputValue){
+    usersContainer.style.display = 'block';
+    users = userInfo.filter(user => {
+      return user.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase());
+    });
+    users = users.map(user => {
+      return`<li>${user}</li>`
+    });
+  } else {
+    usersContainer.style.display = 'none';
   }
-};
-//Creates a new traffic chart
-let trafficChart = new Chart(trafficCanvas, {
-  type: 'line',
-  data: trafficData,
-  options: trafficOptions
+  showUsers(users);
 });
-
-//Objects containing daily chart data.
-
-const dailyData = {
-    labels: ["S", "M", "T", "W", "T", "F", "S"],
-    datasets: [{
-        label: '# of Hits',
-        data: [75, 115, 175, 125, 225, 200, 100],
-        backgroundColor: 'rgb(151, 78, 151)',
-        boarderWidth: 1
-    }]
-};
-
-const dailyOptions = {
-    scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true 
-            }
-        }]
-    },
-    legend: {
-        display: false
-    }
+//Append user's input to ul element if any.
+const showUsers = (list) => {
+  let liForUl = list.join('');
+  if(!list.length) {
+    usersContainer.innerHTML = ``;
+  } else {
+    usersContainer.innerHTML = liForUl;
+//Select all li attach a click event to each    
+    const ulChildren = usersContainer.querySelectorAll('li');
+    ulChildren.forEach(li => {
+      li.addEventListener('click', (e) => {
+//When click the become the input text content        
+        formInput.value = li.textContent;
+        usersContainer.style.display = 'none';
+      })
+    })
+  }
 }
-
-let dailyChart = new Chart(dailyCanvas, {
-    type: 'bar',
-    data: dailyData,
-    options: dailyOptions
-});
-
-//Object containing data for mobile charts
-const mobileData = {
-    labels: ["Desktop", "Tablet", "Phones"],
-    datasets: [{
-        label: '# of Users',
-        data: [2000, 550, 500],
-        borderWidth: 0,
-        backgroundColor: [
-            'purple',
-            'rgb(233, 186, 233)',
-            'rgb(151, 78, 151)',
-        ]
-    }]
-};
-
-const mobileOptions = {
-    legend: {
-        position: 'right',
-        labels: {
-            boxWidth: 20,
-            fontstyle: 'bold'
-        }
-    }
-}
-
-//Creates a new mobile chart.
-let mobileChart = new Chart(mobileCanvas, {
-    type: 'doughnut',
-    data: mobileData,
-    options: mobileOptions
-});
-
