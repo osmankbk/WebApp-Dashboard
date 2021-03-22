7a94d1e2daef14b6c48deef2dff268cb1c6d637d
osmankbk <mackiekbk@yahoo.com>
2021-03-22 13:42:49 -0400

    Clean up & align code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 //Get required elements
-
 //Store alert div
 const alertBanner = document.querySelector(".alert");
 //Store notifications div
@@ -32,11 +31,9 @@ const timeZone = document.querySelector('#timezone');
 const save = document.querySelector('.save');
 //Store settings cancle button
 const cancle = document.querySelector('.cancle');
-
-
 //Function that gives content to the alert div in the htmll
 const seeNotifications = () => {
-  notifications.innerHTML = `<div class="notification-sub">
+	notifications.innerHTML = `<div class="notification-sub">
   <div class="messages">
     <p>You have 6 unread messages</p>
     <p class="close">x</p>
@@ -52,44 +49,38 @@ const seeNotifications = () => {
     <p class="close">x</p>
   </div> 
   </div>`
-  return notifications;
+	return notifications;
 }
 //Call seeNotifications function to populate the notifications div on load.
 seeNotifications();
 //Store notification message container in notification
 const notification = document.querySelector('.notification-sub');
-
-
 //Click event on container
 notification.addEventListener('click', (e) => {
-  const element = e.target;
-  if (element.classList.contains('close')) {
-      element.parentNode.remove();
-  }
+	const element = e.target;
+	if (element.classList.contains('close')) {
+		element.parentNode.remove();
+	}
 });
-
 //Click event displaying messages & hiding(make it transparent) the notification div.
 bell.addEventListener('click', () => {
-  notification.style.display = 'block';
-  notification.classList.add('showing');
-  notifications.style.backgroundColor = 'transparent';
+	notification.style.display = 'block';
+	notification.classList.add('showing');
+	notifications.style.backgroundColor = 'transparent';
 });
-
 //Window click event to get raid of notification div when clicked outside of it.
 window.addEventListener('click', (e) => {
-  if(e.target !== bell && notification.classList.contains('showing')) {
-    notification.style.display = 'none';
-  }
-  if (e.target.classList.contains('close')) {
-    notification.style.display = 'block';
-}
+	if (e.target !== bell && notification.classList.contains('showing')) {
+		notification.style.display = 'none';
+	}
+	if (e.target.classList.contains('close')) {
+		notification.style.display = 'block';
+	}
 });
-
-
 //Function that gives content to the alert div in the htmll
 const callAlertBanner = () => {
-    //Creates the html for alert.
-    alertBanner.innerHTML = `<div class="alert-banner">
+	//Creates the html for alert.
+	alertBanner.innerHTML = `<div class="alert-banner">
     <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
     to complete</p>
     <p class="alert-banner-close">x</p>
@@ -98,100 +89,90 @@ const callAlertBanner = () => {
 callAlertBanner();
 //Click event delegation for alert banner.
 alertBanner.addEventListener('click', (e) => {
-    const element = e.target;
-    if(element.classList.contains("alert-banner-close")){
-        alertBanner.style.display = 'none';
-    }
+	const element = e.target;
+	if (element.classList.contains("alert-banner-close")) {
+		alertBanner.style.display = 'none';
+	}
 });
-
 //Message submit section
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if(!input.value || !textArea.value) {
-    alert("Aren't you missing something? Make sure you select a user and type a message");
-  } else {
-    alert("Your message has been submitted!");
-  }
+	e.preventDefault();
+	if (!input.value || !textArea.value) {
+		alert("Aren't you missing something? Make sure you select a user and type a message");
+	} else {
+		alert("Your message has been submitted!");
+	}
 });
-
 //Auto complete section
 //If key up
 input.addEventListener('keyup', (e) => {
-  //Store input typed users
-  let users = [];
-  let inputValue = e.target.value;
-  if(inputValue){
-    usersContainer.style.display = 'block';
-    users = userInfo.filter(user => {
-      return user.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase());
-    });
-    users = users.map(user => {
-      return`<li>${user}</li>`
-    });
-  } else {
-    usersContainer.style.display = 'none';
-  }
-  showUsers(users);
+	//Store input typed users
+	let users = [];
+	let inputValue = e.target.value;
+	if (inputValue) {
+		usersContainer.style.display = 'block';
+		users = userInfo.filter(user => {
+			return user.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase());
+		});
+		users = users.map(user => {
+			return `<li>${user}</li>`
+		});
+	} else {
+		usersContainer.style.display = 'none';
+	}
+	showUsers(users);
 });
 //Append user's input to ul element if any.
 const showUsers = (list) => {
-    const liForUl = !list.length ? `` : 
-      list.join('');
-      usersContainer.innerHTML = liForUl;
-//Select all li attach a click event to each  
-      const ulChildren = usersContainer.querySelectorAll('li');
-      ulChildren.forEach(li => {
-        li.addEventListener('click', (e) => {
-//When click the become the input text content 
-        formInput.value = li.textContent;
-        usersContainer.style.display = 'none';
-      });
-    });
+	const liForUl = !list.length ? `` : list.join('');
+	usersContainer.innerHTML = liForUl;
+	//Select all li attach a click event to each  
+	const ulChildren = usersContainer.querySelectorAll('li');
+	ulChildren.forEach(li => {
+		li.addEventListener('click', (e) => {
+			//When click the become the input text content 
+			formInput.value = li.textContent;
+			usersContainer.style.display = 'none';
+		});
+	});
 }
-
-
 //Function that retrieves searches from Local Storage, return an array
 const getRecentSettings = () => {
-  const settings = localStorage.getItem('recentSettings');
-  const timeGot = settings ? JSON.parse(settings) : {};
-  return timeGot;
+	const settings = localStorage.getItem('recentSettings');
+	const timeGot = settings ? JSON.parse(settings) : {};
+	return timeGot;
 }
-
 //Set the time zone to the saved value in localStorage on load.
 timeZone.value = getRecentSettings().times;
-
-
 //Function that retrieves toggle history from Local Storage, return an array
-const getRecentToggle= () => {
-  const getToggle = localStorage.getItem('recentToggle');
-  const toggleGot = getToggle ? JSON.parse(getToggle) : timeZone.selectedIndex = 0;
-  return toggleGot;
+const getRecentToggle = () => {
+	const getToggle = localStorage.getItem('recentToggle');
+	const toggleGot = getToggle ? JSON.parse(getToggle) : timeZone.selectedIndex = 0;
+	return toggleGot;
 }
-
 //Set the toggles to the saved values in localStorage on load.
 firstToggleButton.checked = getRecentToggle().first;
 secondToggleButton.checked = getRecentToggle().second;
-
-
 //Click event that saves settings to storage
 save.addEventListener('click', (e) => {
-//Time Zones localStorage 
-  const setting = timeZone.value;
-  const settingsValue = {'times': setting};
-
-  localStorage.setItem('recentSettings', JSON.stringify(settingsValue));
-
-//Toggle buttons localStorage 
-  const firstToggle = firstToggleButton.checked;
-  const secondToggle = secondToggleButton.checked;
-  const toggled = {'first':firstToggle, 'second':secondToggle};
-
-  localStorage.setItem('recentToggle', JSON.stringify(toggled));
+	//Time Zones localStorage 
+	const setting = timeZone.value;
+	const settingsValue = {
+		'times': setting
+	};
+	localStorage.setItem('recentSettings', JSON.stringify(settingsValue));
+	//Toggle buttons localStorage 
+	const firstToggle = firstToggleButton.checked;
+	const secondToggle = secondToggleButton.checked;
+	const toggled = {
+		'first': firstToggle,
+		'second': secondToggle
+	};
+	localStorage.setItem('recentToggle', JSON.stringify(toggled));
 });
-
 //Click event that deletes settings to storage
 cancle.addEventListener('click', (e) => {
-  localStorage.removeItem('recentSettings');
-  localStorage.removeItem('recentToggle');
-  timeZone.selectedIndex = 0;
+	localStorage.removeItem('recentSettings');
+	localStorage.removeItem('recentToggle');
+	timeZone.selectedIndex = 0;
 });
--- js/charts.js
+++ js/charts.js
@@ -4,183 +4,160 @@ const trafficCanvas = document.querySelector('#traffic-chart');
 const dailyCanvas = document.querySelector('.daily-chart');
 //Store canvas element('mobile-chart')
 const mobileCanvas = document.querySelector('.mobile-chart');
-
-
 trafficNav.addEventListener('click', (e) => {
-    if(e.target.tagName === 'LI') {
-      const trafficLi = trafficNav.querySelectorAll('li');
-      trafficLi.forEach( li => {
-        li.classList.remove('selected');
-        switch(e.target.className){
-            case 'daily': 
-             //Creates a new traffic chart
-              trafficChart = new Chart(trafficCanvas, {
-              type: 'line',
-              data: trafficDataDaily,
-              options: trafficOptions
-              });
-              e.target.classList.add('selected');
-            break;
-
-            case 'hourly': 
-            //Creates a new traffic chart
-             trafficChart = new Chart(trafficCanvas, {
-             type: 'line',
-             data: trafficDataHourly,
-             options: trafficOptions
-             });
-             e.target.classList.add('selected');
-           break;
-
-           case 'weekly': 
-           //Creates a new traffic chart
-            trafficChart = new Chart(trafficCanvas, {
-            type: 'line',
-            data: trafficDataWeekely,
-            options: trafficOptions
-            });
-            e.target.classList.add('selected');
-          break;
-
-          case 'monthly': 
-          //Creates a new traffic chart
-           trafficChart = new Chart(trafficCanvas, {
-           type: 'line',
-           data: trafficDataMonthly,
-           options: trafficOptions
-           });
-           e.target.classList.add('selected');
-         break;
-          }
-      });
-    }
-  });
-
-
-
-//Objects containing traffic chart data.
+	if (e.target.tagName === 'LI') {
+		const trafficLi = trafficNav.querySelectorAll('li');
+		trafficLi.forEach(li => {
+			li.classList.remove('selected');
+			switch (e.target.className) {
+				case 'daily':
+					//Creates a new traffic chart
+					trafficChart = new Chart(trafficCanvas, {
+						type: 'line',
+						data: trafficDataDaily,
+						options: trafficOptions
+					});
+					e.target.classList.add('selected');
+					break;
+				case 'hourly':
+					//Creates a new traffic chart
+					trafficChart = new Chart(trafficCanvas, {
+						type: 'line',
+						data: trafficDataHourly,
+						options: trafficOptions
+					});
+					e.target.classList.add('selected');
+					break;
+				case 'weekly':
+					//Creates a new traffic chart
+					trafficChart = new Chart(trafficCanvas, {
+						type: 'line',
+						data: trafficDataWeekely,
+						options: trafficOptions
+					});
+					e.target.classList.add('selected');
+					break;
+				case 'monthly':
+					//Creates a new traffic chart
+					trafficChart = new Chart(trafficCanvas, {
+						type: 'line',
+						data: trafficDataMonthly,
+						options: trafficOptions
+					});
+					e.target.classList.add('selected');
+					break;
+			}
+		});
+	}
+});
+//Objects containing Monthly traffic chart data.
 let trafficDataMonthly = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-      backgroundColor: 'rgb(233, 186, 233)',
-      borderWidth: 1,
-    }]
-  };
-
-//Objects containing traffic chart data.
+	labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+	datasets: [{
+		data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+		backgroundColor: 'rgb(233, 186, 233)',
+		borderWidth: 1,
+	}]
+};
+//Objects containing Daily traffic chart data.
 let trafficDataDaily = {
-  labels: ["S", "M", "T", "W", "T", "F", "S", "S", "M", "T", "W"],
-  datasets: [{
-    data: [2500, 2000, 700, 2500, 1000, 1200, 000, 2000, 2500, 900, 2500],
-    backgroundColor: 'purple',
-    borderWidth: 1,
-  }]
+	labels: ["S", "M", "T", "W", "T", "F", "S", "S", "M", "T", "W"],
+	datasets: [{
+		data: [2500, 2000, 700, 2500, 1000, 1200, 000, 2000, 2500, 900, 2500],
+		backgroundColor: 'purple',
+		borderWidth: 1,
+	}]
 };
-
-//Objects containing traffic chart data.
+//Objects containing Hourly traffic chart data.
 let trafficDataHourly = {
-  labels: ["12AM", "4AM", "6AM", "8AM", "10AM", "12PM", "2PM", "4PM", "8PM", "10PM", "11PM"],
-  datasets: [{
-    data: [300, 2000, 200, 2000, 500, 1200, 6000, 900, 1100, 400, 800],
-    backgroundColor: 'black',
-    borderWidth: 1,
-  }]
+	labels: ["12AM", "4AM", "6AM", "8AM", "10AM", "12PM", "2PM", "4PM", "8PM", "10PM", "11PM"],
+	datasets: [{
+		data: [300, 2000, 200, 2000, 500, 1200, 6000, 900, 1100, 400, 800],
+		backgroundColor: 'black',
+		borderWidth: 1,
+	}]
 };
-
-//Objects containing traffic chart data.
+//Objects containing Weekly traffic chart data.
 let trafficDataWeekely = {
-  labels: ["WK-1", "WK-2", "WK-3", "WK-4", "WK-1", "WK4", "WK-3", "WK-2", "WK-1", "WK-4", "WK-5"],
-  datasets: [{
-    data: [1000, 1600, 900, 1100, 2500, 1200, 1000, 1500, 2000, 1400, 2500],
-    backgroundColor: 'darkred',
-    borderWidth: 1,
-  }]
+	labels: ["WK-1", "WK-2", "WK-3", "WK-4", "WK-1", "WK4", "WK-3", "WK-2", "WK-1", "WK-4", "WK-5"],
+	datasets: [{
+		data: [1000, 1600, 900, 1100, 2500, 1200, 1000, 1500, 2000, 1400, 2500],
+		backgroundColor: 'darkred',
+		borderWidth: 1,
+	}]
 };
-
-  let trafficOptions = {
-    aspectRatio: 2.5,
-    animation: {
-      duration: 800,
-      easing: 'easeOutBack'
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    },
-    legend: {
-      display: false
-    }
-  };
-  //Creates a new traffic chart
-  let trafficChart = new Chart(trafficCanvas, {
-    type: 'line',
-    data: trafficDataMonthly,
-    options: trafficOptions
-  });
-
-  //Objects containing daily chart data.
-  
-  const dailyData = {
-      labels: ["S", "M", "T", "W", "T", "F", "S"],
-      datasets: [{
-          label: '# of Hits',
-          data: [75, 115, 175, 125, 225, 200, 100],
-          backgroundColor: 'rgb(151, 78, 151)',
-          boarderWidth: 1
-      }]
-  };
-  
-  const dailyOptions = {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true 
-              }
-          }]
-      },
-      legend: {
-          display: false
-      }
-  }
-  
-  let dailyChart = new Chart(dailyCanvas, {
-      type: 'bar',
-      data: dailyData,
-      options: dailyOptions
-  });
-  
-  //Object containing data for mobile charts
-  const mobileData = {
-      labels: ["Desktop", "Tablet", "Phones"],
-      datasets: [{
-          label: '# of Users',
-          data: [2000, 550, 500],
-          borderWidth: 0,
-          backgroundColor: [
-              'purple',
-              'rgb(233, 186, 233)',
-              'rgb(151, 78, 151)',
-          ]
-      }]
-  };
-  
-  const mobileOptions = {
-      legend: {
-          position: 'right',
-          labels: {
-              boxWidth: 20,
-              fontstyle: 'bold'
-          }
-      }
-  }
-  
-  //Creates a new mobile chart.
-  let mobileChart = new Chart(mobileCanvas, {
-      type: 'doughnut',
-      data: mobileData,
-      options: mobileOptions
-  });
+let trafficOptions = {
+	aspectRatio: 2.5,
+	animation: {
+		duration: 800,
+		easing: 'easeOutBack'
+	},
+	scales: {
+		yAxes: [{
+			ticks: {
+				beginAtZero: true
+			}
+		}]
+	},
+	legend: {
+		display: false
+	}
+};
+//Creates a new traffic chart
+let trafficChart = new Chart(trafficCanvas, {
+	type: 'line',
+	data: trafficDataMonthly,
+	options: trafficOptions
+});
+//Objects containing daily chart data.
+const dailyData = {
+	labels: ["S", "M", "T", "W", "T", "F", "S"],
+	datasets: [{
+		label: '# of Hits',
+		data: [75, 115, 175, 125, 225, 200, 100],
+		backgroundColor: 'rgb(151, 78, 151)',
+		boarderWidth: 1
+	}]
+};
+const dailyOptions = {
+	scales: {
+		yAxes: [{
+			ticks: {
+				beginAtZero: true
+			}
+		}]
+	},
+	legend: {
+		display: false
+	}
+}
+let dailyChart = new Chart(dailyCanvas, {
+	type: 'bar',
+	data: dailyData,
+	options: dailyOptions
+});
+//Object containing data for mobile charts
+const mobileData = {
+	labels: ["Desktop", "Tablet", "Phones"],
+	datasets: [{
+		label: '# of Users',
+		data: [2000, 550, 500],
+		borderWidth: 0,
+		backgroundColor: ['purple', 'rgb(233, 186, 233)', 'rgb(151, 78, 151)', ]
+	}]
+};
+const mobileOptions = {
+	legend: {
+		position: 'right',
+		labels: {
+			boxWidth: 20,
+			fontstyle: 'bold'
+		}
+	}
+}
+//Creates a new mobile chart.
+let mobileChart = new Chart(mobileCanvas, {
+	type: 'doughnut',
+	data: mobileData,
+	options: mobileOptions
+});
